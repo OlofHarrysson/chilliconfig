079d4205aa5d99b82b1341372df4855b258ff9b7
OlofHarrysson <harrysson.olof@gmail.com>
2019-10-18 20:55:37 +0200

    testing color highlight

--- chilliconfig.py
+++ chilliconfig.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, FrozenInstanceError
 from collections import OrderedDict
 from abc import ABC
 import pprint
+import json
 
 import sys
 from io import StringIO
@@ -10,115 +11,167 @@ import inspect
 import argparse
 import functools
 
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter, TerminalFormatter
+from pygments.formatters import get_formatter_for_filename
+
 
 @dataclass
 class MasterConfig(ABC):
-    config: str
+  # def __init__(self, config):
+  #   self.config = config
+  #   self.freeze_config = True
+  #   print("MASTER FONCIFG INIIIIT")
 
-    # _frozen: bool = True
-    freeze_config: bool = True
+  print("I AM MASTERCONFIG")
+  config: str
 
-    def get_parameters(self):
-        params = vars(self)
-        # print(params)
-        # qweeeee
-        # frozen = params.pop('_frozen')
-        return OrderedDict(sorted(params.items()))
+  # _frozen: bool = True
+  freeze_config: bool = True
 
-    def __str__(self):
-        params = self.get_parameters()
-        return pprint.pformat(dict(self.get_parameters()))
+  def get_parameters(self):
+    params = vars(self)
+    # print(params)
+    # qweeeee
+    # frozen = params.pop('_frozen')
+    return OrderedDict(sorted(params.items()))
 
-    def freeze(self):
-        ''' Freezes object, making it immutable '''
-        def handler(self, name, value):
-            err_msg = (
-                f"Cannot assign to field '{name}'. Config object is frozen. "
-                "Change 'freeze_config' to False if you want a mutable config object"
-            )
-            raise FrozenInstanceError(err_msg)
+  def __str__(self):
+    params = self.get_parameters()
+    params = dict(self.get_parameters())
+    ss = ""
+    for key, val in params.items():
+      # print(key)
+      # print(val)
+      if isinstance(val, str) and '\n' in val:
+        # print(val)
+        # qweeeeee
+        val = highlight(val, PythonLexer(), TerminalFormatter())
+        s = f"{{'{key}': \n{val}}}"
+      else:
+        # s = f'{key}={val}\n'
+        s = pprint.pformat({key: val})
+      ss = f'{ss}{s}\n'
 
-        setattr(MasterConfig, '__setattr__', handler)
+    # return ss
+    print(ss)
+    qwe
+    # return ss
+    # print(params)
+    # return "{" + "\n".join("{!r}: {!r},".format(k, v)
+    #                        for k, v in params.items()) + "}"
+    # return params
+    return pprint.pformat(params)
+
+  def freeze(self):
+    ''' Freezes object, making it immutable '''
+    def handler(self, name, value):
+      err_msg = (
+        f"Cannot assign to field '{name}'. Config object is frozen. "
+        "Change 'freeze_config' to False if you want a mutable config object")
+      raise FrozenInstanceError(err_msg)
+
+    setattr(MasterConfig, '__setattr__', handler)
 
 
 def setup_config(default_config=None):  # TODO: Handle None
-    config_str = parse_args(default_config)
-    config = choose_config(config_str)
-    return config
+  config_str = parse_args(default_config)
+  config = choose_config(config_str)
+  return config
 
 
 def parse_args(default_config):
-    p = argparse.ArgumentParser()
+  p = argparse.ArgumentParser()
 
-    p.add_argument('--config',
-                   type=str,
-                   default=default_config,
-                   help='What config class to choose')
+  p.add_argument('--config',
+                 type=str,
+                 default=default_config,
+                 help='What config class to choose')
 
-    args, _ = p.parse_known_args()
-    return args.config
+  args, _ = p.parse_known_args()
+  return args.config
 
 
 def choose_config(config_str):
-    # Create config object
-    available_configs = get_available_configs()
-    try:
-        config_class_ = available_configs[config_str]
-        config_obj = config_class_(config_str)
-    except KeyError as e:
-        err_msg = f"Config class '{config_str}' wasn't found. Feel free to create it as a new config class or use one of the existing ones -> {set(available_configs)}"
-        raise KeyError(err_msg) from e
+  # Create config object
+  available_configs = get_available_configs()
+  try:
+    config_class_ = available_configs[config_str]
+    config_obj = config_class_(config_str)
+  except KeyError as e:
+    err_msg = f"Config class '{config_str}' wasn't found. Feel free to create it as a new config class or use one of the existing ones -> {set(available_configs)}"
+    raise KeyError(err_msg) from e
 
-    # Overwrite parameters via optional input flags
-    config_obj = overwrite(config_obj)
+  # Overwrite parameters via optional input flags
+  config_obj = overwrite(config_obj)
 
-    # # Freezes config
-    if config_obj.freeze_config:
-        config_obj.freeze()
-    return config_obj
+  # # Freezes config
+  if config_obj.freeze_config:
+    config_obj.freeze()
+  return config_obj
 
 
 def get_available_configs():
-    available_configs = {}
-    for name, obj in inspect.getmembers(sys.modules[__name__]):
-        if inspect.isclass(obj) and issubclass(obj, MasterConfig):
-            available_configs[name] = obj
-    # print(available_configs)
-    available_configs.pop('MasterConfig')
-    return available_configs
+  available_configs = {}
+  for name, obj in inspect.getmembers(sys.modules[__name__]):
+    if inspect.isclass(obj) and issubclass(obj, MasterConfig):
+      available_configs[name] = obj
+  # print(available_configs)
+  available_configs.pop('MasterConfig')
+  return available_configs
 
 
 def overwrite(config_obj):
-    ''' Overwrites parameters with input flags. Function is needed for the
+  ''' Overwrites parameters with input flags. Function is needed for the
   convenience of specifying parameters via a combination of the config classes
   and input flags. '''
-    class NullIO(StringIO):
-        def write(self, txt):
-            pass
+  class NullIO(StringIO):
+    def write(self, txt):
+      pass
 
-    def parse_unknown_flags(**kwargs):
-        return kwargs
+  def parse_unknown_flags(**kwargs):
+    return kwargs
 
-    sys.stdout = NullIO()
-    extra_arguments = fire.Fire(parse_unknown_flags)
-    sys.stdout = sys.__stdout__
+  sys.stdout = NullIO()
+  extra_arguments = fire.Fire(parse_unknown_flags)
+  sys.stdout = sys.__stdout__
 
-    for key, val in extra_arguments.items():
-        if key not in vars(config_obj):
-            err_str = f"The input parameter '{key}' isn't allowed. It's only possible to overwrite attributes that exist in the DefaultConfig class. Add your input parameter to the default class or catch it before this message"
-            raise NotImplementedError(err_str)
-        setattr(config_obj, key, val)
+  for key, val in extra_arguments.items():
+    if key not in vars(config_obj):
+      err_str = f"The input parameter '{key}' isn't allowed. It's only possible to overwrite attributes that exist in the DefaultConfig class. Add your input parameter to the default class or catch it before this message"
+      raise NotImplementedError(err_str)
+    setattr(config_obj, key, val)
 
-    return config_obj
+  return config_obj
 
 
 def config_class(func):
-    class_name = func.__name__
-    err_msg = (f"Can't decorate '{class_name}' of type {type(func)}. "
-               "Can only be used for classes")
-    assert inspect.isclass(func), err_msg
-    err_msg = (f"Can't decorate '{class_name}' since it's not a sublass of "
-               "'chilliconfig.MasterConfig'")
-    assert issubclass(func, MasterConfig), err_msg
-    setattr(sys.modules[__name__], class_name, func)
-    return func
+  class_name = func.__name__
+  err_msg = (f"Can't decorate '{class_name}' of type {type(func)}. "
+             "Can only be used for classes")
+  assert inspect.isclass(func), err_msg
+  err_msg = (f"Can't decorate '{class_name}' since it's not a sublass of "
+             "'chilliconfig.MasterConfig'")
+  assert issubclass(func, MasterConfig), err_msg
+  setattr(sys.modules[__name__], class_name, func)
+  return func
+
+
+def config_class(func):
+  class_name = func.__name__
+  err_msg = (f"Can't decorate '{class_name}' of type {type(func)}. "
+             "Can only be used for classes")
+  assert inspect.isclass(func), err_msg
+  err_msg = (f"Can't decorate '{class_name}' since it's not a sublass of "
+             "'chilliconfig.MasterConfig'")
+  assert issubclass(func, MasterConfig), err_msg
+  setattr(sys.modules[__name__], class_name, func)
+
+  return dataclass(func,
+                   init=True,
+                   repr=False,
+                   eq=False,
+                   order=False,
+                   unsafe_hash=False,
+                   frozen=False)
--- run.py
+++ run.py
@@ -1,47 +1,79 @@
-from chilliconfig import config_class, MasterConfig, setup_config
 # from user_config import OlofConfig
+from chilliconfig import config_class, MasterConfig, setup_config
 import time
 from dataclasses import dataclass
+from typing import Callable, Iterator, Union, Optional, List
+import imgaug as ia
+import imgaug.augmenters as iaa
+import inspect
+
+
+class Transformer():
+  def __init__(self):
+    self.seq = iaa.Resize({"height": 64, "width": 64})
+
+  def __call__(self, im):
+    augmented_im = self.seq.augment_image(np.array(im))
+    return Image.fromarray(augmented_im)
+
+  def __repr__(self):
+    # return repr(self.seq)
+    return str(self.seq)
 
 
 @config_class
 class MainConfig(MasterConfig):
-    def __init__(self, name):
-        super().__init__(name)
-        self.start_time = time.time()
-        self.img_size = 100
-        self.classes = ['car', 'dog']
-        self.freeze_config = False
+  def __init__(self, name):
+    super().__init__(name)
+    print("MAIN CONFIG SUPER")
+    self.start_time = time.time()
+    self.img_size = 100
+    self.classes = ['car', 'dog']
+    self.freeze_config = False
 
 
-@dataclass
+# @dataclass
+class MyClass():
+  # bajs = 100
+  # korv: int = bajs * 2
+  def __init__(self):
+    self.bsize = 100
+
+
+def transforms():
+  return iaa.Sequential([
+    iaa.Crop(px=(0, 16)),
+    iaa.Fliplr(
+      0.5
+    ),  # JUST COMMEENT TO MAKE THIS LINE REALLY LONG GOT DAMNIT WOWOOWOWOWOWOWOWOWOWOOWWOOWOWWOWOWOWOOWOWOW
+    iaa.GaussianBlur(sigma=(0, 3.0))
+  ])
+
+
 @config_class
 class Train(MainConfig):
-    def __init__(self, name):
-        super().__init__(name)
-        self.img_size: int = 350
-
-    img_size: int = 50
-    #     self.img_size = 50
-
-
-@config_class
-class Inference(MainConfig):
-    def __init__(self, name):
-        super().__init__(name)
-        self.classes = ['car']
-
-        # self.complex_data_structure = read_file()
+  def __init__(self, name):
+    super().__init__(name)
+    self.classes = ['dog']
+    # self.class_obj = MyClass()
+    # self.transforms = Transformer()
+    # self.func_transforms = transforms()
+    self.func_transforms = inspect.getsource(transforms)
+    # print(self.func_transforms)
+    # print(type(self.func_transforms))
+    # qwe
 
 
 def main():
-    # config = setup_config()
-    config = setup_config(default_config='Train')
-    print(config)
-    # config.name = 'New name'
-    # print(config)
+  # config = setup_config()
+  config = setup_config(default_config='Train')
+  print(config)
+  # print("\n\n")
+  # print(repr(config))
+  # config.name = 'New name'
+  # print(config)
 
 
 if __name__ == '__main__':
-    # print("BEFORE MAIN")
-    main()
+  print("BEFORE MAIN")
+  main()
